bc300f4ce0d2a76a0eda7fc5107777a53e0d78cd
Bo Petersen <bo.petersen@creuna.dk>
2015-10-24 23:21:28 +0200

    first commit

---
+++ tasks/coverwise_google_sitemap.js
@@ -0,0 +1,49 @@
+/*
+ * grunt-coverwise-google-sitemap
+ * https://github.com/bo.petersen/coverwise-google-sitemap
+ *
+ * Copyright (c) 2015 Bo Petersen
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('coverwise_google_sitemap', 'The best Grunt plugin ever.', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      urlKey: 'url'
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.urlKey));
+
+      // Handle options.
+      //src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(f.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest + '" created.');
+    });
+  });
+
+};
